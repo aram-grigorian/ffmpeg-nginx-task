478001cb5fe2619c0eaef90d3d9bef32bdb71573
ramgrigoryan@protonmail.com <ramgrigoryan@protonmail.com>
2025-10-30 08:00:06 +0400

    Modified keys.tf to reduce rsa_bits to 2048, set a lifecycle policy ignore_changes

--- terraform-infra/keys.tf
+++ terraform-infra/keys.tf
@@ -1,6 +1,10 @@
 resource "tls_private_key" "ec2_key" {
   algorithm = "RSA"
-  rsa_bits  = 4096
+  rsa_bits  = 2048
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "aws_key_pair" "ec2_key_pair" {
